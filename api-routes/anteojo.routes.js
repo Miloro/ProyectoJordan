63f7379c1cff1f60b0c852ed36a4d47910841807
Navani17 <vaninanadia@gmail.com>
2019-06-11 21:48:30 -0300

    ajuste de usuario en anteojo

--- api-routes/anteojo.routes.js
+++ api-routes/anteojo.routes.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
 let anteojoController = require('../controllers/anteojo.controller');
+let mdAutorizacion = require('../middlewares/autorizacion');
+
 
 app.get('/stock', anteojoController.stockDeAnteojo);
-app.get('/stockConPrecioCompra', anteojoController.stockDeAnteojoConPrecioCompra);
+app.get('/stockConPrecioCompra' , mdAutorizacion.soloAdmin(), anteojoController.stockDeAnteojoConPrecioCompra);
 app.patch('/stock', anteojoController.disminuirStock);
 app.post('/', anteojoController.altaModeloAnteojo);
 
